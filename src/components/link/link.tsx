b9c8c9437a458cf0af26d69b4f909b7505f56e6e
Leonardo Lemos <leo.lemos.ds@icloud.com>
2024-03-24 20:56:12 -0300

    refactor: create the link component

---
+++ src/components/link/link.tsx
@@ -0,0 +1,43 @@
+import type { ComponentPropsWithoutRef, JSX } from 'react';
+
+import Next$Link from 'next/link';
+
+import merge from '~/styles/merge';
+
+type Next$LinkProps = ComponentPropsWithoutRef<typeof Next$Link>;
+
+export type LinkVariant = 'primary' | 'secondary' | 'opaque';
+
+export interface LinkProps extends Next$LinkProps {
+	variant?: LinkVariant;
+}
+
+function Link({
+	children,
+	className,
+	variant = 'primary',
+	target = '_blank',
+	...props
+}: LinkProps): JSX.Element {
+	return (
+		<Next$Link
+			{...props}
+			data-variant={variant}
+			target={target}
+			className={merge(
+				'transition-all',
+				'data-[variant="primary"]:text-purple-500 dark:data-[variant="primary"]:text-purple-400',
+				'hover:data-[variant="primary"]:text-purple-600 hover:dark:data-[variant="primary"]:text-purple-300',
+				'data-[variant="secondary"]:text-black dark:data-[variant="secondary"]:text-white',
+				'hover:data-[variant="secondary"]:text-zinc-900 hover:dark:data-[variant="secondary"]:text-zinc-200',
+				'data-[variant="opaque"]:text-zinc-700 dark:data-[variant="opaque"]:text-zinc-400',
+				'hover:data-[variant="opaque"]:text-zinc-800 hover:dark:data-[variant="opaque"]:text-zinc-200',
+				className,
+			)}
+		>
+			{children}
+		</Next$Link>
+	);
+}
+
+export default Link;
