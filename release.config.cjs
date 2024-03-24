1d3bc03d2cc79fc953b6c25c721fa96548445c64
Leonardo Lemos <leo.lemos.ds@icloud.com>
2024-03-24 20:59:19 -0300

    ci: create the GitHub Actions + Semantic Release

---
+++ release.config.cjs
@@ -0,0 +1,37 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+	branches: ['main'],
+	repositoryUrl: 'https://github.com/mrlemoos/mrlemoos.dev',
+	plugins: [
+		[
+			'@semantic-release/commit-analyzer',
+			{
+				preset: 'angular',
+				parserOpts: {
+					noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
+				},
+			},
+		],
+		[
+			'@semantic-release/release-notes-generator',
+			{
+				preset: 'angular',
+				parserOpts: {
+					noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
+				},
+				writerOpts: {
+					commitsSort: ['subject', 'scope'],
+				},
+			},
+		],
+		[
+			'@semantic-release/git',
+			{
+				message:
+					'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+			},
+		],
+	],
+};
